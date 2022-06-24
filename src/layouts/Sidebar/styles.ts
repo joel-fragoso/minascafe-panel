d6e556242c3723868deea89abf8089f3b118196e
felzika <fel_2@live.com>
2022-06-24 19:23:53 -0300

    refactor: Melhora layout
    - Adicionado componente Header e Sidebar ao MainLayout.
    - Adicionado estilos ao Header e Sidebar.

--- src/layouts/Sidebar/styles.ts
+++ src/layouts/Sidebar/styles.ts
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
+import { lighten } from 'polished';
 
 export const Container = styled.aside`
   width: 28rem;
   height: 100%;
   background-color: ${({ theme }) => theme.background.default};
+  border-right: 1px solid
+    ${({ theme }) => lighten(0.08, theme.background.default)};
 `;
