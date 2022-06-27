ffba73a167e1eb6b94bcd17ffa3df2e904f0dcd5
felzika <fel_2@live.com>
2022-06-26 21:18:29 -0300

    feature: Adiciona button
    - Adicionado componente `Button`.
    - Refatorado SignIn para utilizar o componente `Button`.
    
    Closes #14

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,3 @@
-import { lighten } from 'polished';
 import styled from 'styled-components';
 
 export const Container = styled.div`
@@ -9,34 +8,11 @@ export const Container = styled.div`
   height: 100%;
   padding: 0 1.6rem;
 
-  div {
+  form {
     width: 100%;
 
     @media (min-width: 380px) {
       width: 28rem;
-    }
-  }
-
-  button {
-    padding: 0.8rem 1.6rem;
-    font-size: 1.6rem;
-    font-weight: 700;
-    color: ${({ theme }) => theme.background.default};
-    background-color: ${({ theme }) => theme.pallete.primary.main};
-    display: flex;
-    border-radius: 0.4rem;
-    width: 100%;
-    align-items: center;
-    justify-content: center;
-    transition: background-color 0.2s linear;
-
-    &:hover {
-      background-color: ${({ theme }) => theme.pallete.primary.dark};
-    }
-
-    &:active {
-      background-color: ${({ theme }) =>
-        lighten(0.1, theme.pallete.primary.dark as string)};
     }
   }
 `;
