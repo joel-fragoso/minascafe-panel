670b85154f6edea18314b3eb9203f932751d9836
felzika <fel_2@live.com>
2022-06-29 00:26:01 -0300

    refactor: Refatora botão
    - Refatora botão adicionando estilo ao `:focus`.
    
    Closes #48

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -20,6 +20,11 @@ export const Container = styled.button`
       lighten(0.1, theme.pallete.primary.dark as string)};
   }
 
+  &:focus {
+    outline: 2px solid ${({ theme }) => theme.pallete.primary.main};
+    outline-offset: 2px;
+  }
+
   &:disabled {
     cursor: not-allowed;
     background-color: ${({ theme }) => shade(0.4, theme.pallete.primary.main)};
