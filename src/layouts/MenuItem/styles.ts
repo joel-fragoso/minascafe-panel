4092c310cf27c54115ccd3615a2db1887a2b725a
felzika <fel_2@live.com>
2022-08-18 01:11:54 -0300

    hotfix: Retira espaços dos ícons
    - Removido espaços do ícone `Bars` no `Header`.
    - Removido espaços dos ícones do `Sidebar`.
    
    Closes #189

--- src/layouts/MenuItem/styles.ts
+++ src/layouts/MenuItem/styles.ts
@@ -15,29 +15,34 @@ export const Container = styled.li<IContainerProps>`
 
   > a {
     flex: 1;
-    padding: 1.2rem 1.6rem;
+    padding: 1.6rem;
+    display: flex;
+    align-items: center;
     font-family: 'Nunito', sans-serif;
     font-size: 1.4rem;
     color: ${({ theme }) => theme.pallete.primary.main};
     border-radius: 0.8rem;
     transition: background-color 0.3s linear;
     position: relative;
-    line-height: 2.4rem;
 
-    span {
+    > div {
       position: absolute;
-      margin-left: 0.8rem;
-      padding: 1.2rem 1.6rem;
-      border-radius: 0.8rem;
-      top: 0;
+      padding: 1.2rem;
       visibility: hidden;
       transition: background-color 0.3s linear;
+      border-radius: 0.8;
+      margin-left: 2.4rem;
+
+      span {
+        vertical-align: middle;
+      }
 
       ${({ expanded }) =>
         expanded &&
         css`
           position: relative;
           visibility: visible;
+          line-height: 1.2rem;
           padding: 0;
         `}
     }
@@ -45,7 +50,7 @@ export const Container = styled.li<IContainerProps>`
     &:hover {
       background-color: ${({ theme }) => theme.background.paper};
 
-      span {
+      > div {
         z-index: 999;
         visibility: visible;
         background-color: ${({ theme }) => theme.background.paper};
