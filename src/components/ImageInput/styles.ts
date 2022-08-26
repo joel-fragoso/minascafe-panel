368e0f68d2cc026ac0a4c4a589cf0b8dea7255e9
felzika <fel_2@live.com>
2022-08-25 23:08:43 -0300

    hotfix: Ajusta botão do perfil
    - Ajusta o background do botão da imagem no perfil.
    
    Closes #197

--- src/components/ImageInput/styles.ts
+++ src/components/ImageInput/styles.ts
@@ -81,7 +81,6 @@ export const Container = styled.div<ContainerProps>`
     width: 3.6rem;
     height: 3.6rem;
     display: inline-block;
-    background-color: ${({ theme }) => theme.background.default};
     border-radius: 0.8rem;
     cursor: pointer;
   }
