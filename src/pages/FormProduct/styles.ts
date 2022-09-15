5ad813367647adc847077b304eb60e40f180bb71
felzika <fel_2@live.com>
2022-09-15 00:49:24 -0300

    feature: adiciona mascara ao campo preco
    Adiconado uma máscara basica ao compo `Preço`.

--- src/pages/FormProduct/styles.ts
+++ src/pages/FormProduct/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import Input from '../../components/Input';
 
 export const Container = styled.div`
   padding: 1.6rem;
@@ -16,4 +17,20 @@ export const LabelContainer = styled.div`
   display: flex;
   column-gap: 0.8rem;
   align-items: center;
+`;
+
+export const PriceContainer = styled.div`
+  position: relative;
+`;
+
+export const PriceTag = styled.span`
+  position: absolute;
+  top: 1.6rem;
+  left: 1.6rem;
+  color: ${({ theme }) => theme.pallete.secondary?.main};
+  pointer-events: none;
+`;
+
+export const PriceInput = styled(Input)`
+  text-align: right;
 `;
